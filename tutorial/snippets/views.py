f08084a1cb0e7715d62d8c06f5cd6747a34a0e23
Кристина <kristina.shtaynmets@mail.ru>
2023-11-07 23:09:56 +0700

    Реализуем разрешения для аутенцифицированных пользователей

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,11 +2,13 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
    def perform_create(self, serializer):
        serializer.save(owner=self.request.user)
@@ -15,6 +17,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
