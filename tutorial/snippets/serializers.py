7a1c034bc0d3b45795de58226c9433b9d1b20fd1
Кристина <kristina.shtaynmets@mail.ru>
2023-11-07 22:57:56 +0700

    Добавляем код для отображения пользователей

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 from .models import Snippet
 
 
@@ -7,3 +7,11 @@ class SnippetSerializer(serializers.ModelSerializer):
    class Meta:
        model = Snippet
        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.ModelSerializer):
+   snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+   class Meta:
+       model = User
+       fields = ['id', 'username', 'snippets']
